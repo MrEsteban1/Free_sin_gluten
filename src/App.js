2341224ce87326ce6aa4a4548129711f026e92de
MrEsteban1 <estebangonz98@@gmail.com>
2022-03-08 18:43:10 -0300

    Se agrega el itemCount

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React from 'react';
-import Navbar from './components/Navbar/navbar'
-import './App.css';
-import  ItemListContainer  from './components/General/ItemListContainer/ItemListContainer';
-
+import React from "react";
+import Navbar from "./components/Navbar/navbar";
+import "./App.css";
+import ItemListContainer from "./components/General/ItemListContainer/ItemListContainer";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <ItemListContainer title='Alfajor Yuca Sin Tack' 
-      image='https://arcordiezb2c.vteximg.com.br/arquivos/ids/161959-500-400/ALFAJOR-YUKA-DLECHE-STACC-1-6082.jpg?v=637473582852600000' 
-      detail="Es muy rico!!"/>
+      <ItemListContainer
+        title="Alfajor Yuca Sin Tack"
+        image="https://arcordiezb2c.vteximg.com.br/arquivos/ids/161959-500-400/ALFAJOR-YUKA-DLECHE-STACC-1-6082.jpg?v=637473582852600000"
+        detail="Es muy rico!!"
+        stock={5}
+      />
     </div>
   );
 }
