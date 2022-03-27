b616c5f315a4511d5d60bee2321cdad7093ddaed
MrEsteban1 <estebangonz98@@gmail.com>
2022-03-27 02:53:13 -0300

    Avance con el chart

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./components/Navbar/navbar";
 import "./App.css";
 import ItemListContainer from "./components/General/ItemListContainer/ItemListContainer";
 import ItemDetailContainer from "./components/General/ItemDetailContainer/itemDetailContainer";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { ChartContext } from "./contexts/chartContext";
 
 function App() {
+  const [chart, setChart] = useState([]);
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<ItemListContainer />} />
-          <Route path="/products" element={<ItemListContainer />} />
-          <Route path="/recipe/:idItem" element={<ItemDetailContainer />} />
-        </Routes>
-      </BrowserRouter>
+      <ChartContext.Provider value={chart}>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<ItemListContainer />} />
+            <Route path="/products" element={<ItemListContainer />} />
+            <Route path="/recipe/:idItem" element={<ItemDetailContainer />} />
+          </Routes>
+        </BrowserRouter>
+      </ChartContext.Provider>
     </div>
   );
 }
--- src/components/General/ItemListContainer/counter/counter.jsx
+++ src/components/General/ItemListContainer/counter/counter.jsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { useCounter } from "../../../CustomHooks/useCounter";
+import "./style.css";
 
-const Counter = ({counter,increment,decrement,handlerAdd}) => {
+const Counter = ({ counter, increment, decrement, handlerAdd }) => {
   //const { counter, increment, decrement } = useCounter(0);
+  const [addToChart, setAdd] = useState(true);
+
+  const activeAddHandler = () => {
+    let state;
+    if (counter > 0) {
+      state = true;
+      handlerAdd();
+    } else state = false;
+
+    setAdd(state);
+  };
+
   return (
     <>
       <div className="number_items">{counter}</div>
@@ -24,7 +37,12 @@ const Counter = ({counter,increment,decrement,handlerAdd}) => {
           +
         </button>
       </div>
-      <button className="button-addItem" onClick={handlerAdd}>Agregar</button>
+      <button className="button-addItem" onClick={activeAddHandler}>
+        Agregar
+      </button>
+      {!addToChart && (
+        <div className="warning-noItems">No hay productos que agregar.</div>
+      )}
     </>
   );
 };
