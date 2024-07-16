e3d192cdeb515781f93f91ad98340d21938fe569
Esteban <70283738+MrEsteban1@users.noreply.github.com>
2024-07-15 22:42:53 -0300

    Update firebase-config.js

--- src/helpers/firebase-config.js
+++ src/helpers/firebase-config.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAw0KR0Zk0rJs0URStyEqjUTQu7aDNg33U",
-  authDomain: "free-sin-gluten.firebaseapp.com",
-  projectId: "free-sin-gluten",
-  storageBucket: "free-sin-gluten.appspot.com",
-  messagingSenderId: "9468949076",
-  appId: "1:9468949076:web:b35606bf162d9814c6f3c1",
-  measurementId: "G-EJGKKR8M5K",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 const app = initializeApp(firebaseConfig);
